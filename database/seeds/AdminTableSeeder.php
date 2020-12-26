e7b8d736f83105d70bc459fe3b595cd8448d1ddb
Mago <mago@lakaykb.com>
2020-12-26 23:59:27 +0800

    change email address

--- database/seeds/AdminTableSeeder.php
+++ database/seeds/AdminTableSeeder.php
@@ -16,17 +16,17 @@ class AdminTableSeeder extends Seeder
     public function run()
     {
         $admin = Admin::find(6);
-        $admin->email = 'admin@gmail.com';
+        $admin->email = 'admin@dmipark.com';
         $admin->password = Hash::make('administrator');
         $admin->save();
 
         $user = User::find(58);
-        $user->email = 'user@gmail.com';
+        $user->email = 'mago.umandam@gmail.com';
         $user->password = Hash::make('useruser');
         $user->save();
 
         $user2 = User::find(64);
-        $user2->email = 'user2@gmail.com';
+        $user2->email = 'mago@lakaykb.com';
         $user2->password = Hash::make('useruser');
         $user2->save();
     }
